9f54826957e1f3931b5cd4fdeef598e86ed63cbd
drew <me@andrinoff.com>
2025-07-17 12:24:37 +0400

    fix: added checksum values

--- Casks/floatplane.rb
+++ Casks/floatplane.rb
@@ -2,11 +2,11 @@ cask "floatplane" do
   version "1.2.3"
 
   on_arm do
-    sha256 "PASTE_THE_ARM64_SHA256_CHECKSUM_HERE"
+    sha256 "2c5499b91025e7f2b07d9a6bb06df35e3f62250f2992053b36f81aee0d574428"
     url "https://github.com/andrinoff/floatplane/releases/download/v#{version}/Floatplane-#{version}-arm64.dmg"
   end
   on_intel do
-    sha256 "PASTE_THE_X64_SHA256_CHECKSUM_HERE"
+    sha256 "feac1542551b5d952193f06b7784415ee995a9eb37c3f0a0aabfeaed866d60f9"
     url "https://github.com/andrinoff/floatplane/releases/download/v#{version}/Floatplane-#{version}-x64.dmg"
   end
 
