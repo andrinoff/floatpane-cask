eb4c73fb8003a65c457158543e40b5832f4b2ec4
drew <me@andrinoff.com>
2025-07-19 02:13:05 +0400

    fix: Update floatpane.rb

--- Casks/floatpane.rb
+++ Casks/floatpane.rb
@@ -3,16 +3,16 @@ cask "floatpane" do
 
   on_arm do
     sha256 "62a5b16c2ed62f036201d520d4c22d074d30db25d0ea3a86454323e22c6780ee"
-    url "https://github.com/andrinoff/floatpane/releases/download/v#{version}/floatpane-#{version}-arm64.dmg"
+    url "https://github.com/floatpane/floatpane/releases/download/v#{version}/floatpane-#{version}-arm64.dmg"
   end
   on_intel do
     sha256 "ba55a986aa98cec826c77b8811f323b3300c3c84892d9a79ef80a76857a8a641"
-    url "https://github.com/andrinoff/floatpane/releases/download/v#{version}/floatpane-#{version}-x64.dmg"
+    url "https://github.com/floatpane/floatpane/releases/download/v#{version}/floatpane-#{version}-x64.dmg"
   end
 
   name "floatpane"
   desc "A wallpaper manager app for macOS"
-  homepage "https://github.com/andrinoff/floatpane"
+  homepage "https://github.com/floatpane/floatpane"
 
   app "floatpane.app"
 
